f44d7fb1a15f5f594f9ce3148334b5904097c17b
Peace-Makinita <peacemakinita@gmail.com>
2022-06-12 17:08:32 +0200

    tv-app update image slider

--- src/components/navigation/NavigationDrawer.vue
+++ src/components/navigation/NavigationDrawer.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <v-container class="fill-height">
-      <ImageCover/>
-    </v-container>
+    <ImageCover
+      category="Cars"
+      description="Watch all the exciting content content about cars"
+    />
+    <ImageCover
+      category="Food"
+      description="Are you a foodie? you'll enjoy this content"
+    />
+    <ImageCover
+      category="Music"
+      description="Watch and listen to music all the exciting content about music"
+    />
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
+    <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title>SPAN CODING CHALLENGE</v-list-item-title>
@@ -18,12 +23,7 @@
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item
-          v-for="item in getTopicList"
-          :key="item.id"
-          link
-        >
-
+        <v-list-item v-for="item in getTopicList" :key="item.id" link>
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
@@ -34,58 +34,62 @@
 </template>
 
 <script>
-import ImageCover from '../common/ImageCover.vue';
-import {useStore} from 'vuex'
+import ImageCover from "../common/ImageCover.vue";
+import { useStore } from "vuex";
 
-  export default {
-    name: 'NavigationDrawer',
-    components: {
-      ImageCover
+export default {
+  name: "NavigationDrawer",
+  components: {
+    ImageCover,
+  },
+  props: {
+    openNavigation: {
+      type: Boolean,
+      default: false,
     },
-    props: {
-      openNavigation: {
-        type: Boolean,
-        default: false
-      }
-    },
-    data () {
-      return {
-        drawer: null,
-        items: [
-          { title: 'Home', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-forum' },
-        ],
-      }
-    },
+  },
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Home", icon: "mdi-view-dashboard" },
+        { title: "About", icon: "mdi-forum" },
+      ],
+    };
+  },
 
-    async created() {
-      try {
-       await this.$store.dispatch('fetchTopics')
-      } catch(error) {
-        console.log('error in component')
-      }
-    },
-    computed: {
-      movies() {
-    return this.$store.getters.movies
+  async created() {
+    try {
+      await this.$store.dispatch("fetchTopics");
+    } catch (error) {
+      console.log("error in component");
+    }
+  },
+  computed: {
+    movies() {
+      return this.$store.getters.movies;
     },
     genres() {
-      return this.$store.getters.genres
+      return this.$store.getters.genres;
     },
     getTopicList() {
-      return this.$store.state.topics
-    }
+      return this.$store.state.topics;
     },
-    methods: {
-      isDrawerShowing() {
-        this.drawer = !this.drawer
-      console.log('message from parent ??');
-      },
+  },
+  methods: {
+    isDrawerShowing() {
+      this.drawer = !this.drawer;
+      console.log("message from parent ??");
     },
-  }
+  },
+};
 </script>
 
-
+<style scoped>
+.v-navigation-drawer--temporary {
+  z-index: 10;
+}
+</style>
 
 <!-- /* 320px — 480px: Mobile devices
 481px — 768px: iPads, Tablets
--- src/store/index.js
+++ src/store/index.js
@@ -1,51 +1,81 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-      topics: [],
-      isResponseOkay: false
+  state: {
+    topics: [],
+    isResponseOkay: false,
+  },
+
+  getters: {
+    getTopics: (state) => state.topics,
+    getIsResponseOkay: (state) => state.isResponseOkay,
+  },
+
+  actions: {
+    // fetchTopics({ commit }) {
+    //     const url = 'https://api.unsplash.com/topics/?client_id=ucVVGtioEDaoporFMI1stcwOGSFRWE_qZ0hsOjQG9EY'
+
+    //     return new Promise((resolve, reject) => {
+    //         axios.get(url).then((response) => {
+    //                 if (response?.data) {
+    //                     commit('SET_TOPICS', response.data)
+    //                     commit('SET_RESPONSE_OKAY', true)
+    //                     resolve(response.data)
+    //                 } else {
+    //                     commit('SET_TOPICS', [])
+    //                     commit('SET_RESPONSE_OKAY',  false)
+    //                     reject(error)
+    //                 }
+
+    //         }).catch((error) => {
+    //             commit('SET_RESPONSE_OKAY',  false)
+    //             reject(error)
+    //         })
+    //     })
+    //   },
+
+    fetchTopics({ commit }) {
+      commit("SET_TOPICS", [
+        { title: "Food" },
+        { title: "Cars" },
+        { title: "Animals" },
+        { title: "Sea" },
+        { title: "Ocean" },
+        { title: "House" },
+      ]);
+      //const url = 'https://api.unsplash.com/topics/?client_id=ucVVGtioEDaoporFMI1stcwOGSFRWE_qZ0hsOjQG9EY'
+
+      // return new Promise((resolve, reject) => {
+      //     axios.get(url).then((response) => {
+      //             if (response?.data) {
+      //                 commit('SET_TOPICS', response.data)
+      //                 commit('SET_RESPONSE_OKAY', true)
+      //                 resolve(response.data)
+      //             } else {
+      //                 commit('SET_TOPICS', [])
+      //                 commit('SET_RESPONSE_OKAY',  false)
+      //                 reject(error)
+      //             }
+
+      //     }).catch((error) => {
+      //         commit('SET_RESPONSE_OKAY',  false)
+      //         reject(error)
+      //     })
+      // })
     },
+  },
 
-    getters: {
-      getTopics: (state) => state.topics,
-      getIsResponseOkay: (state) => state.isResponseOkay
+  mutations: {
+    SET_TOPICS(state, topics) {
+      state.topics = topics;
     },
-
-    actions: {
-        fetchTopics({ commit }) {
-            const url = 'https://api.unsplash.com/topics/?client_id=ucVVGtioEDaoporFMI1stcwOGSFRWE_qZ0hsOjQG9EY'
-
-            return new Promise((resolve, reject) => {
-                axios.get(url).then((response) => {
-                        if (response?.data) {
-                            commit('SET_TOPICS', response.data)
-                            commit('SET_RESPONSE_OKAY', true)
-                            resolve(response.data)
-                        } else {
-                            commit('SET_TOPICS', [])
-                            commit('SET_RESPONSE_OKAY',  false)
-                            reject(error)
-                        }
-
-                }).catch((error) => {
-                    commit('SET_RESPONSE_OKAY',  false)
-                    reject(error)
-                })
-            })
-          }
-      },
-
-      mutations: {
-        SET_TOPICS(state, topics) {
-          state.topics = topics
-      },
-      SET_RESPONSE_OKAY(state, isResponseOkay) {
-        state.isResponseOkay = isResponseOkay
-      }
-    }
-})
+    SET_RESPONSE_OKAY(state, isResponseOkay) {
+      state.isResponseOkay = isResponseOkay;
+    },
+  },
+});
 
 export default store;
